9b7c03f113f888c06f3852da5536929a33f3184a
darikawaiii <darikawaiii@gmail.com>
2025-02-08 19:16:41 +0100

    First pass at database.py thats works well

--- database.py
+++ database.py
@@ -4,9 +4,22 @@ conexion = sqlite3.connect('productos1.db')  # inicializa y crea la base de dato
 cursor = conexion.cursor()
 print('Base de datos iniciada\n')
 
-conexion.execute('''CREATE TABLE IF NOT EXISTS productos1 EAN INT, nombre TEXT, precio INT''')
-conexion.commit()
-print('Tabla creada\n')
+def crear_tabla():
+    conexion.execute('''CREATE TABLE IF NOT EXISTS productos1(EAN INT, nombre TEXT, precio INT)''')
+    conexion.commit()
+    print('Tabla creada\n')
 
+EAN=int(input("Introduce el EAN: "))
+nombre=str(input("Introduce el nombre: "))
+precio=int(input("Introduce el precio: "))
+
+conexion.execute('''INSERT INTO productos1 VALUES(?,?,?)''', (EAN, nombre, precio))
+conexion.commit()
+cursor=conexion.execute("SELECT * FROM productos1")
+for row in cursor:
+    print(row)
+conexion.execute('''DELETE FROM productos1''')
+conexion.commit()
 conexion.close()
+print('Base de datos cerrada\n')
 
