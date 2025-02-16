6081cca2d5b198d196ab6f21c0ed237f5de0cafa
Dario <dariopagnanelli@outlook.es>
2025-02-16 21:02:45 +0100

    V1.0.0

--- database.py
+++ database.py
@@ -26,12 +26,12 @@ class funciones_db:
         print('Base de datos cerrada\n')
 
     def eliminar_producto_ean(ean):#Elimina un producto de la tabla productos1 por ean
-            conexion.execute('''DELETE FROM productos1 WHERE EAN=?''', (ean,))
+            conexion.execute('''DELETE FROM productos1 WHERE LOWER(EAN) = LOWER(?)''', (ean,))
             conexion.commit()
             print("Producto eliminado\n")
 
     def eliminar_producto_nombre(nombre):#Elimina un producto de la tabla productos1 por nombre
-            conexion.execute('''DELETE FROM productos1 WHERE nombre=?''', (nombre,))
+            conexion.execute('''DELETE FROM productos1 WHERE LOWER(nombre)=LOWER(?)''', (nombre,))
             conexion.commit()
             print("Producto eliminado\n")
 
@@ -40,12 +40,12 @@ class funciones_db:
         seleccion=int(input("\nQue dato conoce del producto a buscar? 1.EAN 2.Nombre :"))#Selecciona el dato por el que se va a buscar el producto
         if seleccion==1:
             ean=int(input("Introduce el EAN: "))
-            cursor=conexion.execute('''SELECT * FROM productos1 WHERE EAN=?''', (ean))
+            cursor=conexion.execute('''SELECT * FROM productos1 WHERE ean LIKE ?''', ('%'+ean+'%'))
             for row in cursor:
                 print(row)
         elif seleccion==2:
             nombre=str(input("Introduce el nombre: "))
-            cursor=conexion.execute('''SELECT * FROM productos1 WHERE nombre=''', (nombre))
+            cursor=conexion.execute('''SELECT * FROM productos1 WHERE nombre LIKE ?''', ('%'+nombre+'%'))
             for row in cursor: 
                 print(row)
         else: 
--- gui.py
+++ gui.py
@@ -20,7 +20,7 @@ class funciones_gui:
     def anadir_inventario(): #ventana donde se estan las opciones para añadir al inventario
         t1=Toplevel(root)
         t1.title('Añadir inventario')
-        t1.geometry('350x250+200+5') # valores y creacion del toplevel frame
+        t1.geometry('450x250+200+5') # valores y creacion del toplevel frame
     
         # Configurar las columnas y filas para que se expandan
         t1.grid_columnconfigure(0, weight=1)
@@ -79,7 +79,7 @@ class funciones_gui:
         """
         t2=Toplevel(root)
         t2.title('Eliminar producto')
-        t2.geometry('350x250+200+5')
+        t2.geometry('450x300+200+5')
         
         # Configurar las columnas y filas para que se expandan
         t2.grid_columnconfigure(0, weight=1)
@@ -96,49 +96,57 @@ class funciones_gui:
         t2.grid_rowconfigure(8, weight=1)
         t2.grid_rowconfigure(9, weight=1)
         
-        nombreProducto=StringVar()
+        nombre=StringVar() #Se definen las variables a manejar 
         ean=StringVar()
         
-        def trace_callback(*args):
-            print({nombreProducto.get()})
-            button1=ttk.Button(t2, text='Eliminar', command=eliminar_producto, state='default')
+        def trace_callback(*args): #funcion para ver valores  introducidos en el campo de nombre y habilitar el boton de eliminar al introducir un valor
+            print({nombre.get()})
+            button1=ttk.Button(t2, text='Eliminar por Nombre', command=eliminar_producto_nombre, state='default')
             button1.grid(column=1, row=5, pady=10)
-        def trace_callback2(*args):
+            
+            
+        def trace_callback2(*args): #idem, pero para el campo de EAN
             print({ean.get()})
-            button2=ttk.Button(t2, text='Eliminar', command=eliminar_producto, state='default')
-            button2.grid(column=1, row=5, pady=10)
+            button2=ttk.Button(t2, text='Eliminar por EAN', command=eliminar_producto_ean, state='default')
+            button2.grid(column=2, row=5, pady=10)
         
         
-        def eliminar_producto():
+        
+        def eliminar_producto_nombre():
             respuesta=messagebox.askyesno(message='¿Está seguro de que desea eliminar el producto?', title='Eliminar producto')
             if respuesta==True:
                 messagebox.showinfo(message='Producto eliminado', title='Producto eliminado')
-                if ean=='':
-                    db.funciones_db.eliminar_producto_nombre(nombre.get())
-                    db.funciones_db.mostrar_productos()
-                elif nombre=='':
-                    db.funciones_db.eliminar_producto_ean(ean.get())
-                    db.funciones_db.mostrar_productos()
-                else:
-                    pass
+                db.funciones_db.eliminar_producto_nombre(nombre.get())
+                db.funciones_db.mostrar_productos()
                 t2.destroy()
             else:
                 messagebox.showinfo(message='Operación cancelada', title='Operación cancelada')
                 t2.destroy()
-    
+        def eliminar_producto_ean():
+            respuesta=messagebox.askyesno(message='¿Está seguro de que desea eliminar el producto?', title='Eliminar producto')
+            if respuesta==True:
+                messagebox.showinfo(message='Producto eliminado', title='Producto eliminado')
+                db.funciones_db.eliminar_producto_ean(ean.get())
+                db.funciones_db.mostrar_productos()
+                t2.destroy()
+            else:
+                messagebox.showinfo(message='Operación cancelada', title='Operación cancelada')
+                t2.destroy()
+        
         label1=ttk.Label(t2, text='Introduzca el nombre del producto a eliminar:')
         label1.grid(column=1,row=1,sticky=(W))
         label2=ttk.Label(t2, text='Introduzca el EAN del producto a eliminar:')
         label2.grid(column=1,row=3,sticky=(W))
-        nombre=ttk.Entry(t2, textvariable=nombreProducto)
-        nombre.grid(column=1, row=2, sticky=(E,W), columnspan=2, padx=30)
+        nombre_entry=ttk.Entry(t2, textvariable=nombre)
+        nombre_entry.grid(column=1, row=2, sticky=(E,W), columnspan=2, padx=30)
         ean_entry=ttk.Entry(t2, textvariable=ean)
         ean_entry.grid(column=1, row=4, sticky=(E,W), columnspan=2, padx=30)
-        button1=ttk.Button(t2, text='Eliminar', command=eliminar_producto, state='disabled')
+        button1=ttk.Button(t2, text='Eliminar por Nombre', command=eliminar_producto_nombre, state='disabled')
         button1.grid(column=1,row=5, pady=10)
-        nombreProducto.trace_add('write', trace_callback) 
+        button3=ttk.Button(t2, text='Eliminar por EAN', command=eliminar_producto_ean, state='disabled')
+        button3.grid(column=2, row=5, pady=10, padx=10, sticky=(W))
+        nombre.trace_add('write', trace_callback) 
         ean.trace_add('write', trace_callback2)
-    
     
     def iniciar_gui():
 
@@ -167,9 +175,43 @@ class funciones_gui:
         button4=StringVar()
 
         button1=ttk.Button(mainframe, text='Añadir inventario', command=funciones_gui.anadir_inventario).grid(column=1, row=3)
-        button2=ttk.Button(mainframe, text='Mostrar todo el inventario', command=algo).grid(column=2, row=3)
+        button2=ttk.Button(mainframe, text='Mostrar todo el inventario', command=funciones_gui.mostrar_inventario).grid(column=2, row=3)
         button3=ttk.Button(mainframe, text='Elminar un producto', command=funciones_gui.eliminar_producto).grid(column=4, row=3)
         button4=ttk.Button(mainframe, text='Buscar un producto', command=algo).grid(column=5,row=3)
         root.mainloop()
+
+    def mostrar_inventario():
+        t3=Toplevel(root)
+        t3.title('Mostrar todo el inventario')
+        t3.geometry('450x300+200+5')
+        
+        t3.grid_columnconfigure(0, weight=1)
+        t3.grid_columnconfigure(1, weight=1)
+        t3.grid_columnconfigure(2, weight=1)
+        t3.grid_rowconfigure(0, weight=1)
+        t3.grid_rowconfigure(1, weight=1)
+        t3.grid_rowconfigure(2, weight=1)
+        t3.grid_rowconfigure(3, weight=1)
+        t3.grid_rowconfigure(4, weight=1)
+        t3.grid_rowconfigure(5, weight=1)
+        t3.grid_rowconfigure(6, weight=1)
+        t3.grid_rowconfigure(7, weight=1)
+        t3.grid_rowconfigure(8, weight=1)
+        t3.grid_rowconfigure(9, weight=1)
+        
+        tree=ttk.Treeview(t3,columns=('EAN', 'Nombre', 'Cantidad'), show='headings')
+        tree.heading('EAN', text='EAN')
+        tree.heading('Nombre', text='Nombre')
+        tree.heading('Cantidad', text='Cantidad')
+        tree.grid(column=1, row=1, sticky=(N,S,E,W))
+        
+        scroll=ttk.Scrollbar(t3, orient='vertical', command=tree.yview)
+        tree.configure(yscrollcommand=scroll.set)
+        scroll.grid(column=2, row=1, sticky=(N,S))
+        
+        cursor=db.conexion.execute('SELECT * FROM productos1')
+        for row in cursor:
+            tree.insert('', 'end', values=row)
+        
 
 funciones_gui.iniciar_gui()
